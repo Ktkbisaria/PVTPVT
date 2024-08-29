3d2ac72a7e271aea33530549a7a188404772aa0a
Ktkbisaria <ktkbisaria100@example.com>
2024-08-30 01:03:19 +0530

    againremoved download button link

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -9,9 +9,9 @@ function DownLoadResumeBtn() {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
     
-    <Link href={"https://docs.google.com/document/d/1ls-VWWCvsUzc3sJydnQ2GZF9ujGI4HH66CLjIDWlizQ/edit?usp=sharing"} target="_blank" rel="noopener noreferrer" download>
+    
         <HackerBtn label='My Resume' />
-      </Link>
+      
   </div>
   )
 }
