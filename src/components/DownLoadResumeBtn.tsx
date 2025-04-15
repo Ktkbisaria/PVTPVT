b4b585695bb489d5e959f54abba3ce6aa5615e33
Ktkbisaria <ktkbisaria100@example.com>
2025-04-15 23:01:19 +0530

    more added reusme link works

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -5,14 +5,19 @@ import React from 'react'
 import { buttonVariants } from './ui/button'
 import HackerBtn from './HackerBtn'
 
+
+
 function DownLoadResumeBtn() {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
-    
-    
-        <HackerBtn label='My Resume' />
-      
-  </div>
+<a
+  href="https://drive.google.com/file/d/1H5QASTFkmE1hzp0XdozPZllp61S6982V/view?usp=sharing"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <HackerBtn label="My Resume" />
+</a>
+    </div>
   )
 }
 
