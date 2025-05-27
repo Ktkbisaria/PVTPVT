b7bf4d0263f2e80f984d9cd2ed5b3ae754dc9dcb
Ktkbisaria <ktkbisaria100@example.com>
2025-05-28 02:34:57 +0530

    final for now2

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -6,40 +6,7 @@ import { Briefcase } from "lucide-react";
 const educationPage = () => {
   return (
     <div className="h-full w-full relative flex flex-col items-start gap-5 overflow-hidden">
-      {/* Education Section */}
-      <Badge className="gap-2">
-        <Briefcase className="h-5 w-5" />
-        Education
-      </Badge>
-      <div className="flex flex-col gap-3">
-        <Heading>My Education</Heading>
-      </div>
-      <div className="w-full h-fit flex flex-col">
-        <div className="w-full h-fit flex">
-          <FramerWrapper
-            y={0}
-            x={-100}
-            delay={0.35}
-            className="w-1/4 font-rubik flex items-center justify-evenly text-lg max-sm:text-base"
-          >
-            July 2021 - 2025
-          </FramerWrapper>
-          <div className="relative w-3/4 flex">
-            <div className="border-l-4 border-l-[#3c3c3c] pl-4 w-full gap-3  relative">
-              {/* No dot here as education is completed */}
-              <div className="text-2xl font-rubik max-sm:text-xl">
-                B.Tech in Computer Science and Engineering, <br /> University of Petroleum & Energy, UK
-              </div>
-              <p className="font-poppins text-base w-full text-primary max-sm:text-xs">
-                I have completed B.Tech in Computer Science and Engineering from the University of Petroleum & Energy Studies.
-                It equipped me with a strong foundation in computer science principles, programming languages, and problem-solving skills.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Experience Section */}
+            {/* Experience Section */}
       <Badge className="gap-2">
         <Briefcase className="h-5 w-5" />
         Experience
@@ -98,6 +65,40 @@ const educationPage = () => {
           </div>
         </div>
       </div>
+      {/* Education Section */}
+      <Badge className="gap-2">
+        <Briefcase className="h-5 w-5" />
+        Education
+      </Badge>
+      <div className="flex flex-col gap-3">
+        <Heading>My Education</Heading>
+      </div>
+      <div className="w-full h-fit flex flex-col">
+        <div className="w-full h-fit flex">
+          <FramerWrapper
+            y={0}
+            x={-100}
+            delay={0.35}
+            className="w-1/4 font-rubik flex items-center justify-evenly text-lg max-sm:text-base"
+          >
+            July 2021 - 2025
+          </FramerWrapper>
+          <div className="relative w-3/4 flex">
+            <div className="border-l-4 border-l-[#3c3c3c] pl-4 w-full gap-3  relative">
+              {/* No dot here as education is completed */}
+              <div className="text-2xl font-rubik max-sm:text-xl">
+                B.Tech in Computer Science and Engineering, <br /> University of Petroleum & Energy, UK
+              </div>
+              <p className="font-poppins text-base w-full text-primary max-sm:text-xs">
+                I have completed B.Tech in Computer Science and Engineering from the University of Petroleum & Energy Studies.
+                It equipped me with a strong foundation in computer science principles, programming languages, and problem-solving skills.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 };
